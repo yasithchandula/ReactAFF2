180371352b58d62bd22ea629fb60fd97bc1484d5
Yasith Chandula <76646376+yasithchandula@users.noreply.github.com>
2022-05-21 16:06:21 +0530

    css-dev

--- src/Pages/blank.js
+++ src/Pages/blank.js
@@ -1,11 +1,32 @@
 import React from "react";
 import SideNav from "../components/SideNav/SideNav";
+import "./blank.css";
 
 export default function Blank() {
-    return (
-        <>
-            <SideNav />
-            <h1>Test page</h1>
-        </>
-    )
+  return (
+    <div className="Index">
+      <div className="LeftBar">
+        <SideNav />
+      </div>
+      <div className="Content">
+        <div className="TabBar">
+          <div className="TabCard">
+            <div className="LeftCard">
+              <div className="LableCard">
+                <b>New Sales</b>
+              </div>
+              <div className="Count">
+                <h4>123</h4>
+              </div>
+            </div>
+            <div className="RightCard">Image</div>
+          </div>
+          <div className="TabCard"></div>
+          <div className="TabCard"></div>
+          <div className="TabCard"></div>
+        </div>
+        <div className="ContentSec"></div>
+      </div>
+    </div>
+  );
 }
